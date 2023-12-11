c99c62f4039fe9b4614ab127596c2e9fa2500081
Josh <josh_decime@yahoo.com>
2023-12-11 16:54:27 -0700

    Minimal styling

--- app/models/Trivia.js
+++ app/models/Trivia.js
@@ -12,20 +12,27 @@ export class TriviaModel {
 
     get TriviaCard() {
         return `
-    <div class="card col-12 m-1">
-        <div class="row">
-            <h3 class="text-center">${this.category}</h3>
-            <h5 class="text-center">${this.difficulty}</h5>
-            <p class="fs-4 text-center">${this.question}</p>
+<div class="card col-12 m-1">
+    <div class="row">
+        <h3 class="text-center">${this.category}</h3>
+        <h5 class="text-center">${this.difficulty}</h5>
+        <p class="fs-4 text-center">${this.question}</p>
+    </div>
+    <div class="row d-flex justify-content-center pb-2">
+        <div class="col-6 text-center">
+            <button class="btn btn-secondary p-1">${this.correct_answer}</button>
         </div>
-        <div class="row d-flex justify-content-center">
-            <div class="col-6 text-center">${this.correct_answer}</div>
-            <div class="col-6 text-center">${this.incorrect_answers[0]}</div>
-            <div class="col-6 text-center">${this.incorrect_answers[1]}</div>
-            <div class="col-6 text-center">${this.incorrect_answers[2]}</div>
-
+        <div class="col-6 text-center p-1">
+            <button class="btn btn-secondary">${this.incorrect_answers[0]}</button>
+        </div>
+        <div class="col-6 text-center p-1">
+            <button class="btn btn-secondary">${this.incorrect_answers[1]}</button>
+        </div>
+        <div class="col-6 text-center p-1">
+            <button class="btn btn-secondary">${this.incorrect_answers[2]}</button>
         </div>
     </div>
+</div>
         `
     }
 }
